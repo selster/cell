73b6b91dca75aa2c4ddec6ca1d283e427f7caaf4
blackfruitapps <blackfruitapps@gmail.com>
2018-12-24 10:34:01 -0800

    Initial Commit

---
+++ app/src/main/java/com/xlstocsv/MainActivity.java
@@ -0,0 +1,220 @@
+package com.xlstocsv;
+// importing packages
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.obsez.android.lib.filechooser.ChooserDialog;
+import com.xlstocsv.utils.DatabaseHandler;
+import com.xlstocsv.utils.Utils;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+
+// what the android is going to start with
+public class MainActivity extends AppCompatActivity {
+    // defining variables
+    private static final int PICKFILE_REQUEST_CODE = 1; //
+    private Button pickFile; // here is the famous button
+    private ArrayList<String> columnNames = new ArrayList<>();
+    private ArrayList<String> values = new ArrayList<>();
+    Context context;
+    private final String TAG = this.getClass().getName();
+    private int counter = 0;
+    public static String PACKAGE_NAME;
+    //Path that will open the file picker from. If it doesnt exist, it will crash
+    private String excelFolderPath = "/storage/emulated/0/notif/excel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PACKAGE_NAME = getApplicationContext().getPackageName();
+        initwidgets();
+
+    }
+
+    private void initwidgets() {
+        context = this;
+        pickFile = (Button) findViewById(R.id.pickFile_Button);
+
+        pickFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestPermission();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICKFILE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImageURI = data.getData(); // a uri is a path
+                String filepath = "";
+                filepath = Utils.getPath(this, selectedImageURI);
+
+                Log.d("Test", "Excel File :" + filepath);
+                try {
+                    Log.d("onActivityResult: ", filepath);
+                    readFile(filepath); // here we try to read filepath
+                                        // this method is below
+                                        // private void readFile(
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InvalidFormatException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private String getFileUri(Intent data) {
+        String filePath = null;
+        Uri _uri = data.getData();
+        Log.d("","URI = "+ _uri);
+        if (_uri != null && "content".equals(_uri.getScheme())) {
+            Cursor cursor = this.getContentResolver().query(_uri, new String[] { android.provider.MediaStore.Images.ImageColumns.DATA }, null, null, null);
+            cursor.moveToFirst();
+            filePath = cursor.getString(0);
+            cursor.close();
+        } else {
+            filePath = _uri.getPath();
+        }
+        return filePath;
+       // Log.d("","Chosen path = "+ filePath);
+    }
+    private void requestPermission() {
+        Dexter.withActivity(this) // dexter has to do with permission
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+
+                            new ChooserDialog().with(MainActivity.this)
+                                    .withFilter(false, false, "xls")
+                                    .withStartFile(excelFolderPath)
+                                    .withChosenListener(new ChooserDialog.Result() {
+                                        @Override
+                                        public void onChoosePath(String path, File pathFile) {
+                                            try {
+                                                readFile(pathFile.getPath());
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            } catch (InvalidFormatException e) {
+                                                e.printStackTrace();
+                                            }
+                                            Toast.makeText(MainActivity.this, "FILE: " + path, Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                                    .build()
+                                    .show();
+
+
+                            //Old Code
+
+//                            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                            Uri uri = Uri.parse(excelFolderPath);
+//                            Log.d("Test", "Setting File Picker to : " +  uri.toString());
+//                            intent.setDataAndType(uri, "application/vnd.ms-excel");
+//                            intent.addCategory(Intent.CATEGORY_OPENABLE);
+//                            startActivityForResult(Intent.createChooser(intent,
+//                                    "Choose XLS File"), PICKFILE_REQUEST_CODE);
+                        } else if (report.isAnyPermissionPermanentlyDenied()) {
+//                            showPermissionsAlert();
+                        }
+                    }
+
+// /storage/emulated/0/Notification History Log/excel/
+// /storage/emulated/0/NotificationHistory/0/6/log/excel/
+// /storage/emulated/0/NotificationHistory/0/log/excel/
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    } // dexter permission complete here
+
+    private void readFile(String filePath) throws IOException, InvalidFormatException {
+        // this is where we read a file in filepath variable
+
+        // Creating a Workbook object from an Excel file (.xls or .xlsx)
+        // and the owrkbook is called filepath
+        Workbook workbook = WorkbookFactory.create(new File(filePath));
+
+        // 2. Or you can use a for-each loop, there might be > 1 sheet
+        // System.out.println("Retrieving Sheets using for-each loop");
+        for (Sheet sheet : workbook) {
+            System.out.println("=> " + sheet.getSheetName());
+        }
+
+        // Getting the Sheet at index zero, get tghe 1st sheet
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+
+        // 2. Or you can use a for-each loop to iterate over the rows and columns
+        // System.out.println("\n\nIterating over Rows and Columns using for-each loop\n");
+        columnNames.clear();
+        for (Row row : sheet) {
+            System.out.print(row.getRowNum() + "\t");
+            for (Cell cell : row) {
+                String cellValue = dataFormatter.formatCellValue(cell);
+
+                if (row.getRowNum() == 0) {
+                    String colName = cellValue; //NOTIFICATION TITLe
+                    cellValue = cellValue.replaceAll(" ", "_");
+                    Log.d("ColumnName", cellValue);
+
+                    columnNames.add(cellValue);
+                } else {
+                    values.add(cellValue);
+                }
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+        // createTable will make an sqlite database/table in DatabaseHandler
+        // this will make a table?
+        DatabaseHandler.getInstance(context).createTable(columnNames);
+        // this will add both field names and append values
+        DatabaseHandler.getInstance(context).addContact(columnNames, values);
+        //
+        DatabaseHandler.getInstance(context).getAllContacts();
+        // now the excel is closed
+        workbook.close();
+    }
+}
---
+++ app/src/main/java/com/xlstocsv/utils/DatabaseHandler.java
@@ -0,0 +1,193 @@
+package com.xlstocsv.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import com.xlstocsv.MainActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    // my_db = "test" + month + day -->>  table_12_22_2018.db
+    private static String my_db = "";
+   // private static final String DATABASE_NAME = "contactsManager.db";
+    private static final String TABLE_CONTACTS = "contacts";
+    private static final String Dummy = "dummy";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PH_NO = "phone_number";
+    public static DatabaseHandler handler = null;
+
+    private String databasePath = "";
+    //    private static final String DATABASE_PATH = Environment.getExternalStorageDirectory().getPath()
+//            + File.separator + "NotificationHistory/0/6/log/excel/" + DATABASE_NAME;
+    private static final String DATABASE_FOLDER = Environment.getExternalStorageDirectory().getPath()
+            // xls files are written to here
+            // /storage/emulated/0/Notification History0/1Log/Excel
+            + File.separator + "NotificationHistory/0/6/log/excel/";  // looks up excel files?
+    private static final String DATABASE_FOLDER_out = Environment.getExternalStorageDirectory().getPath()
+            + File.separator + "NotificationHistory/0/6/log/excel/"; // where it will write files
+    public static final String EXCEL_FILES = Environment.getExternalStorageDirectory().getPath()
+            + File.separator + "Notification History0/";
+
+    private DatabaseHandler(Context context) {
+        super(context, DATABASE_FOLDER_out + getMyDatabaseName(), null, DATABASE_VERSION);
+
+     //   super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        databasePath = context.getDatabasePath(my_db).getPath();
+        Log.d("Test", "Database Folder :" + DATABASE_FOLDER_out);
+        Log.d("Test","Database Path :" +  databasePath);
+        Log.d("Test", my_db);
+    }
+
+    private static String getMyDatabaseName() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        String year = String.valueOf(calendar.get(Calendar.YEAR));
+        String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
+        String day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        String time = simpleDateFormat.format(calendar.getTime());
+
+        String date_time = year + "_" + month + "_" + day + "_" + time;
+        String my_db_table = "table" + "_" + date_time + ".db";
+        my_db = my_db_table;
+        return my_db_table;
+    }
+//    private DatabaseHandler(Context context) {
+//        super(context, DATABASE_PATH, null, DATABASE_VERSION);
+//    }
+
+    public static DatabaseHandler getInstance(Context context) {
+        if (handler == null) {
+            File databasePath = new File(DATABASE_FOLDER);
+            if (!databasePath.exists())
+                databasePath.mkdirs();
+            handler = new DatabaseHandler(context);
+        }
+        return handler;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public void createTable(ArrayList<String> columns) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        StringBuilder builder = new StringBuilder();
+        String deleteTable = "DROP TABLE IF EXISTS " + TABLE_CONTACTS;
+        Log.e("deleteTable: ", deleteTable);
+        db.execSQL(deleteTable);
+        String createTable = "CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS + "(";
+        builder.append("");
+        builder.append(createTable);
+        for (int i = 0; i < columns.size(); i++) {
+            if (i < columns.size() - 1)
+                builder.append(columns.get(i) + " TEXT,");
+            else
+                builder.append(columns.get(i) + " TEXT)");
+        }
+        Log.e("createTable: ", builder.toString());
+        db.execSQL(builder.toString());
+
+//        Log.d("DatabasePath", databasePath);
+//        String query_Table = "select * from " + TABLE_CONTACTS;
+//        Cursor cursor = db.rawQuery(query_Table)
+//        db.execSQL(query_Table);
+//        Log.e(query_Table);
+//        return builder.toString();
+    }
+
+    public void addContact(ArrayList<String> columns, ArrayList<String> list) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("delete from " + TABLE_CONTACTS);
+        ContentValues values = new ContentValues();
+
+        int i = 0;
+        for (int j = 0; j < list.size(); j++) {
+            if (i < columns.size()) {
+                values.put(columns.get(i), list.get(j));
+                i = i + 1;
+                if (i == columns.size()) {
+                    db.insert(TABLE_CONTACTS, null, values);
+                    i = 0;
+                }
+            }
+        }
+
+        //2nd argument is String containing nullColumnHack
+        db.close();
+        exportDatabase();
+    }
+
+    //    // code to get all contacts in a list view
+    public Cursor getAllContacts() {
+        String selectQuery = "SELECT  * FROM " + TABLE_CONTACTS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Log.e("0: ", cursor.getString(0));
+                Log.e("1: ", cursor.getString(1));
+                Log.e("2: ", cursor.getString(2));
+                Log.e("3: ", cursor.getString(3));
+                Log.e("4: ", cursor.getString(4));
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return cursor;
+    }
+
+    public void exportDatabase() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "//data//" + MainActivity.PACKAGE_NAME + "//databases//" + my_db + "";
+                String backupDBPath = my_db;
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(DATABASE_FOLDER, my_db);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
